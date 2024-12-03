fc18d72bb4e34488c61bc8a808e9886ef9ca3c17
obed-tc <castroobed53@gmail.com>
2024-12-02 20:19:18 -0400

    arregado

--- Program.cs
+++ Program.cs
@@ -5,14 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-
 var app = builder.Build();
-
-// Configurar la aplicación para escuchar en el puerto asignado
-app.Urls.Add($"http://*:{port}");
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,6 +18,7 @@ if (!app.Environment.IsProduction())
 {
     app.UseHttpsRedirection();
 }
+
 
 var summaries = new[]
 {
